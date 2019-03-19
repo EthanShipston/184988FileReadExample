18704ec46c2db17e06defeb9a1a21bfcc3db6e77
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2019-03-19 10:03:28 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184988FileReadExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("BirthdayList.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + "\n";
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnButton2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("BirthdayList2.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    string fullName = sr.ReadLine();
+                    string[] names =
+                        fullName.Split(new char[] { ',' });
+                    lblOutput.Content += names[1] + " " + names[0] + "\n";
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
